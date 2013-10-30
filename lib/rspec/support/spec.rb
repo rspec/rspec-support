6572d6cbf27a0c6e3d3182851ff7e00aaf840a6d
Jon Rowe <hello@jonrowe.co.uk>
2013-10-30 12:08:43 +1100

    stderr splitter to allow us to fail builds on warnings

--- lib/rspec/support/spec.rb
+++ lib/rspec/support/spec.rb
@@ -1,4 +1,7 @@
 require 'rspec/support/spec/deprecation_helpers'
+require 'rspec/support/spec/stderr_splitter'
+
+$stderr = RSpec::Support::StdErrSplitter.new($stderr)
 
 RSpec.configure do |c|
   c.include RSpecHelpers
