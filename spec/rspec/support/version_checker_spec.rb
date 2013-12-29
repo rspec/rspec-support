10eb22e8c197e81dab4ae303410ab9ff181a7142
Andy Lindeman <andy@andylindeman.com>
2013-12-29 12:13:26 -0500

    Adds VersionChecker

---
+++ spec/rspec/support/version_checker_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'rspec/support/version_checker'
+
+module RSpec::Support
+  describe VersionChecker do
+    it 'raises an error if the major version is too low' do
+      checker = VersionChecker.new('foo', '0.7.3', '1.0.0')
+      expect { checker.check_version! }.to raise_error(LibraryVersionTooLowError)
+    end
+
+    it 'raises an error if the minor version is too low' do
+      checker = VersionChecker.new('foo', '1.0.99', '1.1.3')
+      expect { checker.check_version! }.to raise_error(LibraryVersionTooLowError)
+    end
+
+    it 'raises an error if the patch version is too low' do
+      checker = VersionChecker.new('foo', '1.0.8', '1.0.10')
+      expect { checker.check_version! }.to raise_error(LibraryVersionTooLowError)
+    end
+
+    it 'does not raise an error or print a warning when the version is above the min version' do
+      checker = VersionChecker.new('foo', '2.0.0', '1.0.0')
+      expect(Kernel).not_to receive(:warn)
+      checker.check_version!
+    end
+  end
+end
