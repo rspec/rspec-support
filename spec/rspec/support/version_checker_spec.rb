da671f59396a65f72ba212e145834e67de3c5018
Andy Lindeman <andy@andylindeman.com>
2014-02-09 19:43:26 -0500

    Nothing is a warning in this code, remove the check

--- spec/rspec/support/version_checker_spec.rb
+++ spec/rspec/support/version_checker_spec.rb
@@ -19,8 +19,7 @@ module RSpec::Support
       expect { check_version('some_gem', '1.0.8', '1.0.10') }.to raise_error(LibraryVersionTooLowError)
     end
 
-    it 'does not raise an error or print a warning when the version is above the min version' do
-      expect(Kernel).not_to receive(:warn)
+    it 'does not raise an error when the version is above the min version' do
       check_version('some_gem', '2.0.0', '1.0.0')
       check_version('some_gem', '1.2.0', '1.1.0')
       check_version('some_gem', '1.1.3', '1.1.1')
