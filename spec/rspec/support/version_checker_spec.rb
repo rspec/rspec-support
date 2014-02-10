4d4da00c9bc58eeede85e7dae7e7bd3fcec6a0c9
Andy Lindeman <andy@andylindeman.com>
2014-02-09 19:33:07 -0500

    Extracts checking to a method

--- spec/rspec/support/version_checker_spec.rb
+++ spec/rspec/support/version_checker_spec.rb
@@ -3,25 +3,25 @@ require 'rspec/support/version_checker'
 
 module RSpec::Support
   describe VersionChecker do
+    def check_version(*args)
+      VersionChecker.new(*args).check_version!
+    end
+
     it 'raises an error if the major version is too low' do
-      checker = VersionChecker.new('foo', '0.7.3', '1.0.0')
-      expect { checker.check_version! }.to raise_error(LibraryVersionTooLowError)
+      expect { check_version('some_gem', '0.7.3', '1.0.0') }.to raise_error(LibraryVersionTooLowError)
     end
 
     it 'raises an error if the minor version is too low' do
-      checker = VersionChecker.new('foo', '1.0.99', '1.1.3')
-      expect { checker.check_version! }.to raise_error(LibraryVersionTooLowError)
+      expect { check_version('some_gem', '1.0.99', '1.1.3') }.to raise_error(LibraryVersionTooLowError)
     end
 
     it 'raises an error if the patch version is too low' do
-      checker = VersionChecker.new('foo', '1.0.8', '1.0.10')
-      expect { checker.check_version! }.to raise_error(LibraryVersionTooLowError)
+      expect { check_version('some_gem', '1.0.8', '1.0.10') }.to raise_error(LibraryVersionTooLowError)
     end
 
     it 'does not raise an error or print a warning when the version is above the min version' do
-      checker = VersionChecker.new('foo', '2.0.0', '1.0.0')
       expect(Kernel).not_to receive(:warn)
-      checker.check_version!
+      check_version('some_gem', '2.0.0', '1.0.0')
     end
   end
 end
