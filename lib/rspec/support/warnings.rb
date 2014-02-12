b03788937bb5a461d500ad8cea3cadcc4109cf25
Stephen Best <bestie@gmail.com>
2014-02-12 13:00:08 +0000

    Extract warning methods into module

--- lib/rspec/support/warnings.rb
+++ lib/rspec/support/warnings.rb
@@ -1,42 +1,38 @@
 module RSpec
+  module Support
+    module Warnings
+      extend self
 
-  unless respond_to?(:deprecate)
+      def deprecate(deprecated, options = {})
+        warn_with "DEPRECATION: #{deprecated} is deprecated.", options
+      end
 
-    # @private
-    #
-    # Used internally to print deprecation warnings
-    # when rspec-core isn't loaded
-    def self.deprecate(deprecated, options = {})
-      warn_with "DEPRECATION: #{deprecated} is deprecated.", options
+      # @private
+      #
+      # Used internally to print deprecation warnings
+      # when rspec-core isn't loaded
+      def warn_deprecation(message)
+        warn_with "DEPRECATION: \n #{message}"
+      end
+
+      # @private
+      #
+      # Used internally to print warnings
+      def warning(text, options={})
+        warn_with "WARNING: #{text}.", options
+      end
+
+      # @private
+      #
+      # Used internally to print longer warnings
+      def warn_with(message, options = {})
+        call_site = options.fetch(:call_site) { CallerFilter.first_non_rspec_line }
+        message << " Use #{options[:replacement]} instead." if options[:replacement]
+        message << " Called from #{call_site}." if call_site
+        ::Kernel.warn message
+      end
     end
   end
 
-  unless respond_to?(:warn_deprecation)
-
-    # @private
-    #
-    # Used internally to print deprecation warnings
-    # when rspec-core isn't loaded
-    def self.warn_deprecation(message)
-      warn_with "DEPRECATION: \n #{message}"
-    end
-  end
-
-  # @private
-  #
-  # Used internally to print warnings
-  def self.warning(text, options={})
-    warn_with "WARNING: #{text}.", options
-  end
-
-  # @private
-  #
-  # Used internally to print longer warnings
-  def self.warn_with(message, options = {})
-    call_site = options.fetch(:call_site) { CallerFilter.first_non_rspec_line }
-    message << " Use #{options[:replacement]} instead." if options[:replacement]
-    message << " Called from #{call_site}." if call_site
-    ::Kernel.warn message
-  end
-
+  extend RSpec::Support::Warnings
 end
