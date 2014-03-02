7a763edb1f3913557359ef361cfc41f1f7bedf31
Jon Rowe <hello@jonrowe.co.uk>
2014-03-03 03:51:08 +1100

    Extract matchers from rspec-expectations
    These matchers are from rspec-expectations but would be useful for
    testing rspec-mocks too.
    
    They are only intended for internal usage, so make a good candidate
    for extraction.

---
+++ lib/rspec/support/spec/matchers.rb
@@ -0,0 +1,15 @@
+module RSpec
+  module Matchers
+    def fail
+      raise_error(RSpec::Expectations::ExpectationNotMetError)
+    end
+
+    def fail_with(message)
+      raise_error(RSpec::Expectations::ExpectationNotMetError, message)
+    end
+
+    def fail_matching(message)
+      raise_error(RSpec::Expectations::ExpectationNotMetError, /#{Regexp.escape(message)}/)
+    end
+  end
+end
