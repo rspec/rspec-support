4bc18a812ceee67d86a6d36b1d5048d5421fde1f
Myron Marston <myron.marston@gmail.com>
2014-10-23 08:59:50 -0700

    On AppVeyor we get permissions errors from mktmpdir.

--- lib/rspec/support/spec/with_isolated_directory.rb
+++ lib/rspec/support/spec/with_isolated_directory.rb
@@ -1,8 +1,9 @@
 require 'tmpdir'
+Dir.mkdir("./tmp") unless File.directory?("./tmp")
 
 RSpec.shared_context "isolated directory", :isolated_directory => true do
   around do |ex|
-    Dir.mktmpdir do |tmp_dir|
+    Dir.mktmpdir(nil, "./tmp") do |tmp_dir|
       Dir.chdir(tmp_dir, &ex)
     end
   end
