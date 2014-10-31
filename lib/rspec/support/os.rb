b79d33adbd811e59944229fa9b5797219fc7cf7f
Myron Marston <myron.marston@gmail.com>
2014-10-31 00:32:32 -0700

    Remove file that is no longer needed.
    This is a followup to #119 and rspec/rspec-core#1758.

--- lib/rspec/support/os.rb
+++
@@ -1,4 +0,0 @@
-# This file is temporary, just so the rspec-core build passes for now.
-# We should remove it once rspec-core has been updated to
-# require `rspec/support/ruby_features` instead.
-require 'rspec/support/ruby_features'
