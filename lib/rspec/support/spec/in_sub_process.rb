9f47a9cc7bd1b14db154a6d7133ae66adb35aef1
Yuji Nakayama <nkymyj@gmail.com>
2017-04-15 10:05:57 +0900

    Let #in_sub_process helper return the result to main process

--- lib/rspec/support/spec/in_sub_process.rb
+++ lib/rspec/support/spec/in_sub_process.rb
@@ -3,40 +3,57 @@ module RSpec
     module InSubProcess
       if Process.respond_to?(:fork) && !(Ruby.jruby? && RUBY_VERSION == '1.8.7')
 
+        UnmarshableObject = Struct.new(:error)
+
         # Useful as a way to isolate a global change to a subprocess.
 
         # rubocop:disable MethodLength
         def in_sub_process(prevent_warnings=true)
-          readme, writeme = IO.pipe
+          exception_reader, exception_writer = IO.pipe
+          result_reader, result_writer = IO.pipe
 
           pid = Process.fork do
-            exception = nil
             warning_preventer = $stderr = RSpec::Support::StdErrSplitter.new($stderr)
 
             begin
-              yield
+              result = yield
               warning_preventer.verify_no_warnings! if prevent_warnings
-            rescue Support::AllExceptionsExceptOnesWeMustNotRescue => e
-              exception = e
+              # rubocop:disable Lint/HandleExceptions
+            rescue Support::AllExceptionsExceptOnesWeMustNotRescue => exception
+              # rubocop:enable Lint/HandleExceptions
             end
 
-            writeme.write Marshal.dump(exception)
+            exception_writer.write marshal_dump_with_unmarshable_object_handling(exception)
+            exception_reader.close
+            exception_writer.close
 
-            readme.close
-            writeme.close
+            result_writer.write marshal_dump_with_unmarshable_object_handling(result)
+            result_reader.close
+            result_writer.close
+
             exit! # prevent at_exit hooks from running (e.g. minitest)
           end
 
-          writeme.close
+          exception_writer.close
+          result_writer.close
           Process.waitpid(pid)
 
-          exception = Marshal.load(readme.read)
-          readme.close
-
+          exception = Marshal.load(exception_reader.read)
+          exception_reader.close
           raise exception if exception
+
+          result = Marshal.load(result_reader.read)
+          result_reader.close
+          result
         end
         # rubocop:enable MethodLength
         alias :in_sub_process_if_possible :in_sub_process
+
+        def marshal_dump_with_unmarshable_object_handling(object)
+          Marshal.dump(object)
+        rescue TypeError => error
+          Marshal.dump(UnmarshableObject.new(error))
+        end
       else
         def in_sub_process(*)
           skip "This spec requires forking to work properly, " \
