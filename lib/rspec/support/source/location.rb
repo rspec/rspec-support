12b446da4830a72f41bcf601dc50370b2de3fb41
Yuji Nakayama <nkymyj@gmail.com>
2017-05-03 18:55:17 +0900

    Make Source::Location comparable

--- lib/rspec/support/source/location.rb
+++ lib/rspec/support/source/location.rb
@@ -4,8 +4,16 @@ module RSpec
       # @private
       # Represents a source location of node or token.
       Location = Struct.new(:line, :column) do
+        include Comparable
+
         def self.location?(array)
           array.is_a?(Array) && array.size == 2 && array.all? { |e| e.is_a?(Integer) }
+        end
+
+        def <=>(other)
+          line_comparison = (line <=> other.line)
+          return line_comparison unless line_comparison == 0
+          column <=> other.column
         end
       end
     end
