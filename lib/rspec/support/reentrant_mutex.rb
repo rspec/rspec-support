8bb78a71b05cd8bfdd01490024f7caa942a3102d
Phil Pirozhkov <hello@fili.pp.ru>
2020-11-13 00:54:48 +0300

    Prefer Ruby core Mutex to our local copy

--- lib/rspec/support/reentrant_mutex.rb
+++ lib/rspec/support/reentrant_mutex.rb
@@ -6,16 +6,12 @@ module RSpec
     # Based on Monitor as of 2.2 -
     # https://github.com/ruby/ruby/blob/eb7ddaa3a47bf48045d26c72eb0f263a53524ebc/lib/monitor.rb#L9
     #
-    # Depends on Mutex, but Mutex is only available as part of core since 1.9.1:
-    #   exists - http://ruby-doc.org/core-1.9.1/Mutex.html
-    #   dne    - http://ruby-doc.org/core-1.9.0/Mutex.html
-    #
     # @private
     class ReentrantMutex
       def initialize
         @owner = nil
         @count = 0
-        @mutex = Mutex.new
+        @mutex = Thread::Mutex.new
       end
 
       def synchronize
@@ -39,23 +35,6 @@ module RSpec
         @owner = nil
         @mutex.unlock
       end
-    end
-
-    if defined? ::Mutex
-      # On 1.9 and up, this is in core, so we just use the real one
-      class Mutex < ::Mutex
-        # If you mock Mutex.new you break our usage of Mutex, so
-        # instead we capture the original method to return Mutexs.
-        NEW_MUTEX_METHOD = Mutex.method(:new)
-
-        def self.new
-          NEW_MUTEX_METHOD.call
-        end
-      end
-    else # For 1.8.7
-      # :nocov:
-      RSpec::Support.require_rspec_support "mutex"
-      # :nocov:
     end
   end
 end
