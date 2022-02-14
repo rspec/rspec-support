14370ee10244fd693ff7749eabf579bda7030c8e
Igor Victor <gogainda@yandex.ru>
2022-02-14 14:39:04 +0100

    Clean up ruby check

--- spec/rspec/support/spec/stderr_splitter_spec.rb
+++ spec/rspec/support/spec/stderr_splitter_spec.rb
@@ -86,7 +86,7 @@ RSpec.describe 'RSpec::Support::StdErrSplitter' do
     splitter.verify_no_warnings!
   end
 
-  unless defined?(RUBY_ENGINE) && (RUBY_ENGINE == 'rbx' || RUBY_ENGINE == 'truffleruby')
+  unless Ruby.rbx? || Ruby.truffleruby?
     # TruffleRuby doesn't support warnings for now
     # https://github.com/oracle/truffleruby/issues/2595 
     it 'will fail an example which generates a warning' do
