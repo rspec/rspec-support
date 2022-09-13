70cf46ce11a760ee2071eb3c8efca5effcdc4e0c
Jon Rowe <hello@jonrowe.co.uk>
2022-09-13 09:28:54 +0100

    Fix typography mistakes

--- lib/rspec/support/with_keywords_when_needed.rb
+++ lib/rspec/support/with_keywords_when_needed.rb
@@ -8,7 +8,7 @@ module RSpec
 
       module_function
 
-      # Remove this in RSpec 4 in favour of explictly passed in kwargs where
+      # Remove this in RSpec 4 in favour of explicitly passed in kwargs where
       # this is used. Works around a warning in Ruby 2.7
       def class_exec(klass, *args, &block)
         if MethodSignature.new(block).has_kw_args_in?(args)
